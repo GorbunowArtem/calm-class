7ec56e36694a8351c1eb13b44ce90e7c7161b334
ahorbunov-epam <Artem_Horbunov1@epam.com>
2026-09-04 19:39:34 +0300

    feat: configure NuGet package sources and project SDK version while updating unit test implementation

--- tests/unit/CalmClass.ApplicationTests.Unit/Test.cs
+++ tests/unit/CalmClass.ApplicationTests.Unit/Test.cs
@@ -1,11 +1,19 @@
 namespace CalmClass.ApplicationTests.Unit;
 
+using CalmClass.Application;
+
 public class Test
 {
     [Test]
-    public async Task SampleTest() =>
+    public async Task IsEven_WhenValueIsEven_ReturnsTrue()
+    {
         // Arrange
+        var sut = new DummyService();
+
         // Act
+        var result = sut.IsEven(2);
+
         // Assert
-        await Assert.That(true).IsTrue();
+        await Assert.That(result).IsTrue();
+    }
 }
